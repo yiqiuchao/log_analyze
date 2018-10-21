235dd5f973550a5fb3c4864de67ec8a4b8127aca
Charlie.Yi <yiqiuchao@gmail.com>
2018-10-21 13:00:01 +1100

    Fixed formatter.

--- log_parser.py
+++ log_parser.py
@@ -6,9 +6,11 @@ import common
 import regexs
 from collections import defaultdict
 from operator import itemgetter
+import signal
+import multiprocessing
 import argparse
 
-logger = common.init_logger()
+g_logger = common.init_logger()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dir', type=str, default='./',
@@ -76,21 +78,43 @@ def save_to_memory(message_container, loginfo, file_index):
             items.append((value, key))
     
     index = int(0)
+    if not loginfo.endswith('\n'):
+        loginfo += '\n'
     message_container.append("logfile[%d]: %s" % (file_index, loginfo))
     for item in sorted(items):
         name = item[1]
         index += 1
-        message_container.append("Device[%d]\t%20s\t%s" % (index, devices[name], name))
+        message_container.append("Device[%d]\t%20s\t%s\n" % (index, devices[name], name))
         for line in messages[name]:
             message_container.append('\t%s' % line)
+        if not message_container[-1].endswith('\n'):
+            message_container.append("\n")
         message_container.append("="*120)
         message_container.append("\n")
 
+g_shared_list = multiprocessing.Manager().list()
+
+def mp_handler(worker, params, num_of_threads):
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    pool = multiprocessing.Pool(processes=num_of_threads)
+    signal.signal(signal.SIGINT, original_sigint_handler)
+    try:
+        res = pool.map_async(worker, params)
+        # waiting for a long time to make sure it's done its jobs.
+        res.get(1000000)
+    except KeyboardInterrupt:
+        g_logger.debug("Caught KeyboardInterrupt, terminating workers")
+        pool.terminate()
+    else:
+        g_logger.debug("Normal termination")
+        pool.close()
+    pool.join()
+
 
 # {key=name, value=IP}, we assume that both IP and name are unique.
-devices  = defaultdict(str)
+# devices  = defaultdict(str)
 # {key=name, value=list of logs}
-messages = defaultdict(list)
+# messages = defaultdict(list)
 
 message_container = []
 
@@ -98,27 +122,27 @@ logfile = 'logfile'
 if args.prev:
     logfile = 'prev_' + logfile
 
-index = int(1)
 if args.status_file:
     with open('status_code.txt', 'r') as fd:
+        index = int(0)
         for line in fd:
+            index += 1
             logdir = os.path.dirname(line.split()[2])
-            devices.clear()
-            messages.clear()
+            devices  = defaultdict(str)
+            messages = defaultdict(list)
             for file in os.listdir(logdir):
                 if re.match(logfile, file):
                     parse(os.path.join(logdir, file))
             # save(fd_out, logdir, index)
             save_to_memory(message_container, line, index)
             # save_to_memory_without_order(message_container, line, index)
-            index += 1
         # print >> fd_out, "Total logs processed: %d" % index
     message_container.append("Total logs processed: %d" % index)
 else:
     for file in os.listdir(args.dir):
         if re.match(logfile, file):
             parse(args.dir+file)
-    save_to_memory(message_container, args.dir, index)
+    save_to_memory(message_container, args.dir, 0)
 
 with open('log_parser.txt', 'w') as fd_out:
     for item in message_container:
